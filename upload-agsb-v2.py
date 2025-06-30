580126712dc8f67252be039b960c7a50f8f298be
poonyma <138734368+poonyma@users.noreply.github.com>
2025-06-30 13:12:00 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytdlala"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+USER_NAME = "s1agv2"         # 用户名
+UUID = "f51fc463-217f-411f-aae0-4aff24c4ef33"                     # UUID，留空则自动生成
+PORT = 52520                   # Vmess端口，留空或0则自动生成
+DOMAIN = "streamlit.leziren.eu.org"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiZmQ3NmNiODBiZTBhOWMzMzY0MjE1Mzg1NTUwM2RkZDEiLCJ0IjoiZjZlZjEwZGUtOGI5My00ZDgyLTg5MGMtNmIxOTE0NTEwMThkIiwicyI6IllqUmxOek16WldFdE9UVmpPUzAwWXpFNUxXRXlPVE10WmpBNU1HRmpZak5pTWprMCJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
